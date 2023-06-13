b27dc0278703ec7130a2ed8f1bbc49dbe907e4f9
dohitdeegwal <76277872+dohitdeegwal@users.noreply.github.com>
2023-06-13 14:43:28 +0530

    cors()

--- app.ts
+++ app.ts
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(authenticate());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors);
+app.use(cors());
 
 
 // app.use('/awards', awards);
