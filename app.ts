d5b2f2760fa568652efe84e69190ab501edc58d7
dohitdeegwal <76277872+dohitdeegwal@users.noreply.github.com>
2023-07-01 12:51:41 +0530

    removed express.json

--- app.ts
+++ app.ts
@@ -36,7 +36,6 @@ app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
 
 app.use(cors());
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(verifyAdmin);
 app.use(authenticate());
